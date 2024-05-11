d0e5e7ba4b2d23fb4612d52579256e71b98ca773
favor-ui <ferplore@gmail.com>
2024-05-12 00:56:36 +0100

    first commit

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # wellcome_page
 Site Will Coming Soon
-https://rahmanminhaj.github.io/wellcome_page/
--- js/main.js
+++ js/main.js
@@ -1,38 +1,43 @@
- jQuery(document).ready(function($){
-  document.querySelector(".year").innerHTML = new Date().getFullYear();
+// 
+jQuery(function($) {
+  // Set current year
+  $(".year").text(new Date().getFullYear());
 
-  // init 
-    $('.slideVisible').on('init', function () {
-        $('.slideVisible').css({"visibility" : "visible"});
-    });
-    // slider content
-    $('.slider-content').slick({
-      dots: false,
-      arrows: false,
-      infinite: true,
-      slidesToShow: 1,
-      slidesToScroll: 1,
-    });
-    //button slide event
-    $('a[data-slide]').click(function(e) {
-     e.preventDefault();
-     var slideno = $(this).data('slide');
-     $('.slider-content').slick('slickGoTo', slideno - 1);
-   });
-    // background images changes     
-    var images = ["bg1.jpg", "bg2.jpg", "bg3.jpg", "bg4.jpg", "bg5.jpg"];
-    $(function () {
-      var i = 0;
-      $("#particles-js").css("background-image", "url(images/" + images[i] + ")");
-      setInterval(function () {
-        i++;
-        if (i == images.length) {
-          i = 0;
-        }
-        $("#particles-js").fadeOut("slow", function () {
-          $(this).css("background-image", "url(images/" + images[i] + ")");
-          $(this).fadeIn("slow");
-        });
-      }, 6000);
-    });
+  // Initialize slider visibility
+  $('.slideVisible').on('init', function () {
+    $('.slideVisible').css({"visibility" : "visible"});
   });
+
+  // Initialize slider content
+  $('.slider-content').slick({
+    dots: false,
+    arrows: false,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  });
+
+  // Button slide event
+  $('a[data-slide]').click(function(e) {
+    e.preventDefault();
+    var slideno = $(this).data('slide');
+    $('.slider-content').slick('slickGoTo', slideno - 1);
+  });
+
+  // Commented out background image changes
+  /*
+  var images = ["bg1.jpg", "bg2.jpg", "bg3.jpg", "bg4.jpg", "bg5.jpg"];
+  var i = 0;
+  $("#particles-js").css("background-image", "url(images/" + images[i] + ")");
+  setInterval(function () {
+    i++;
+    if (i == images.length) {
+      i = 0;
+    }
+    $("#particles-js").fadeOut("slow", function () {
+      $(this).css("background-image", "url(images/" + images[i] + ")");
+      $(this).fadeIn("slow");
+    });
+  }, 6000);
+  */
+});
